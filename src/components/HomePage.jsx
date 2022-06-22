a8fd2bc45b827c23d9b61178716819a7c1f2736b
peterfoo22 <psuh22@gmail.com>
2022-06-22 10:40:56 +0200

    empty state and home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -7,78 +7,57 @@ import {
   Stack,
   Link,
   Heading,
+  EmptyState,
 } from "@shopify/polaris";
+import ProductList from "./ProductList";
 
-import trophyImgUrl from "../assets/home-trophy.png";
+import { Fragment } from "react";
 
-import { ProductsCard } from "./ProductsCard";
+import React, { useState } from "react";
+
+import { ResourcePicker } from "@shopify/app-bridge-react";
 
 export function HomePage() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  const handleProductSelection = (payload) => {
+    setIsOpen(false);
+    setProducts(payload.selection);
+  };
+
   return (
-    <Page fullWidth>
-      <Layout>
-        <Layout.Section>
-          <Card sectioned>
-            <Stack
-              wrap={false}
-              spacing="extraTight"
-              distribution="trailing"
-              alignment="center"
-            >
-              <Stack.Item fill>
-                <TextContainer spacing="loose">
-                  <Heading>Nice work on building a Shopify app 🎉</Heading>
-                  <p>
-                    Your app is ready to explore! It contains everything you
-                    need to get started including the{" "}
-                    <Link url="https://polaris.shopify.com/" external>
-                      Polaris design system
-                    </Link>
-                    ,{" "}
-                    <Link url="https://shopify.dev/api/admin-graphql" external>
-                      Shopify Admin API
-                    </Link>
-                    , and{" "}
-                    <Link
-                      url="https://shopify.dev/apps/tools/app-bridge"
-                      external
-                    >
-                      App Bridge
-                    </Link>{" "}
-                    UI library and components.
-                  </p>
-                  <p>
-                    Ready to go? Start populating your app with some sample
-                    products to view and test in your store.{" "}
-                  </p>
-                  <p>
-                    Learn more about building out your app in{" "}
-                    <Link
-                      url="https://shopify.dev/apps/getting-started/add-functionality"
-                      external
-                    >
-                      this Shopify tutorial
-                    </Link>{" "}
-                    📚{" "}
-                  </p>
-                </TextContainer>
-              </Stack.Item>
-              <Stack.Item>
-                <div style={{ padding: "0 20px" }}>
-                  <Image
-                    source={trophyImgUrl}
-                    alt="Nice work on building a Shopify app"
-                    width={120}
-                  />
-                </div>
-              </Stack.Item>
-            </Stack>
-          </Card>
-        </Layout.Section>
-        <Layout.Section secondary>
-          <ProductsCard />
-        </Layout.Section>
-      </Layout>
-    </Page>
+    <Fragment>
+      <ResourcePicker
+        resourceType="Product"
+        onCancel={() => setIsOpen(false)}
+        onSelection={handleProductSelection}
+        open={isOpen}
+      ></ResourcePicker>
+
+      {products.length > 0 ? (
+        <Page
+          title="Product Selector"
+          primaryAction={{
+            content: "Select Product",
+            onAction: () => {
+              setIsOpen(true);
+            },
+          }}
+          fullWidth
+        ></Page>
+      ) : (
+        <EmptyState
+          heading="Manage Your Products You want to Display"
+          action={{
+            content: "Select products",
+            onAction: () => setIsOpen(true),
+          }}
+          image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
+        >
+          <p>Select the products you want to use on your banner</p>
+        </EmptyState>
+      )}
+    </Fragment>
   );
 }
