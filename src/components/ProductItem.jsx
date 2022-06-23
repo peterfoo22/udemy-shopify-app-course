94065d62ab483391320fc54217f8958ce8322993
peterfoo22 <psuh22@gmail.com>
2022-06-23 11:20:46 +0200

    master-update spacing

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,15 +1,17 @@
-import { Thumbnail } from '@shopify/polaris'
-import React from 'react'
-import { Fragment } from 'react'
+import { Thumbnail } from "@shopify/polaris";
+import React from "react";
+import { Fragment } from "react";
 import { ImagesMajor } from "@shopify/polaris-icons";
-const ProductItem = ({product}) => {
 
+const ProductItem = ({ product }) => {
   const image = product.images[0] ? product.images[0].originalSrc : ImagesMajor;
 
-  return <Fragment>
-    <Thumbnail source = {image}  />
-    {product.title}
-  </Fragment>
-}
+  return (
+    <Fragment>
+      <Thumbnail source={image} />
+      {product.title}
+    </Fragment>
+  );
+};
 
-export default ProductItem
+export default ProductItem;
